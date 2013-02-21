dae19333fb02ab7606ade6c5761ffc052e41ae99
Thomas Gilgenast <thomasgilgenast@gmail.com>
2013-02-21 07:10:48 -0500

    tab link script take 2

--- assets/js/handle_tab_links.js
+++ assets/js/handle_tab_links.js
@@ -1,31 +1,10 @@
-function handleTabLinks() {
-  var hash = window.location.href.split("#")[1];
-  if (hash !== undefined) {
-    var hpieces = hash.split("/");
-    for (var i=0;i<hpieces.length;i++) {
-      var domelid = hpieces[i];
-      var domitem = $('a[href=#' + domelid + '][data-toggle=tab]');
-      if (domitem.length > 0) {
-        if (i+1 == hpieces.length) {
-          // last piece
-          setTimeout(function() {
-            // Highly unclear why this code needs to be inside a timeout call.
-            // Possibly due to the fact that the first ?.tag('show') call needs
-            // to have it's animation finishing before the next call is being
-            // made.
-            domitem.tab('show');
-          },
-          // This magic timeout is based on trial and error. I bumped it
-          // partially to catch the visitor's attention.
-          1000);
-        } else {
-          domitem.tab('show');
-        }
-      }
-    }
-  }
-}
+// Javascript to enable link to tab
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+} 
 
-jQuery.ready(function() {
-  handleTabLinks();
-}
+// Change hash for page-reload
+$('.nav-tabs a').on('shown', function (e) {
+    window.location.hash = e.target.hash;
+})
